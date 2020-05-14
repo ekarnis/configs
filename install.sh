ab1263d6a7680f9c1bdacc88d30f7546edec2727
Eric Karnis <eric@karnis.ca>
2020-05-14 19:08:11 -0400

    Added arch packages for new installs

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!usr/bin/bash
+
+pacman -S fzf filezilla code yay zsh chromium gimp darktable nodejs keepass nmap chromium darktable gimp htop lutris postgresql pgadmin4 steam tilix
+
+yay -S gitkraken
