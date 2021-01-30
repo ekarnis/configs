3c3777673dd934ba8870785a5f1e81865eb961a3
Eric Karnis <eric@karnis.ca>
2021-01-30 16:00:15 -0500

    Added some packages

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!usr/bin/bash
 
-pacman -S fzf filezilla code yay zsh chromium gimp darktable nodejs keepass nmap chromium darktable gimp lutris postgresql pgadmin4 steam alacritty
+pacman -S fzf filezilla code yay zsh chromium gimp darktable nodejs keepass nmap chromium darktable gimp lutris postgresql pgadmin4 steam alacritty ttf-fira-code nodejs npm github-cli tor torbrowser-launcher docker docker-compose libreoffice-fresh
 
-yay -S gitkraken ytop
+yay -S gitkraken ytop spotify
+
+npm install -g yarn
+
+systemctl start docker.service
+systemctl enable  docker.service
+
+#vs code
+# firacode
+
+#set up oh my zsh
